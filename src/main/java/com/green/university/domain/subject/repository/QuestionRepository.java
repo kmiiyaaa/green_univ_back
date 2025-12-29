6436d12e1670e4a9dc5980b9a90d39aca5d0bae2
yyrrcc <music_v@naver.com>
2025-12-29 18:52:16 +0900

    question 테이블 삭제

--- src/main/java/com/green/university/domain/subject/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.green.university.domain.subject.repository;
-
-import com.green.university.domain.subject.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-
-}
