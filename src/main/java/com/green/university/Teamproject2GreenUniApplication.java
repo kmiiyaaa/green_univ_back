3b35cbd7900600b0fdf816859cfc1e6443c3c4d1
kmiiyaaa <ckm1533@naver.com>
2025-12-30 14:51:16 +0900

    한국시간 추가

--- src/main/java/com/green/university/Teamproject2GreenUniApplication.java
+++ src/main/java/com/green/university/Teamproject2GreenUniApplication.java
@@ -1,12 +1,22 @@
 package com.green.university;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class Teamproject2GreenUniApplication {
+
+    // 한국시간
+    @PostConstruct
+    public void started() {
+        // 애플리케이션의 기본 시간대를 한국(KST)으로 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Teamproject2GreenUniApplication.class, args);
--- src/test/java/com/green/university/Teamproject2GreenUniApplicationTests.java
+++ src/test/java/com/green/university/Teamproject2GreenUniApplicationTests.java
@@ -13,10 +13,5 @@ class Teamproject2GreenUniApplicationTests {
     void contextLoads() {
     }
 
-    // 한국시간
-    @PostConstruct
-    public void started() {
-    // 애플리케이션의 기본 시간대를 한국(KST)으로 설정
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
+
 }
