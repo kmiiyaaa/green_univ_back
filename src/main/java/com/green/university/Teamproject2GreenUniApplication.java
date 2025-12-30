4c5cf31ed64b3c22cbe3ecd21b5ad3901efce364
kmiiyaaa <ckm1533@naver.com>
2025-12-30 15:20:34 +0900

    한국 시간 db

--- src/main/java/com/green/university/Teamproject2GreenUniApplication.java
+++ src/main/java/com/green/university/Teamproject2GreenUniApplication.java
@@ -12,11 +12,11 @@ import java.util.TimeZone;
 public class Teamproject2GreenUniApplication {
 
     // 한국시간
-//    @PostConstruct
-//    public void started() {
-//        // 애플리케이션의 기본 시간대를 한국(KST)으로 설정
-//        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-//    }
+    @PostConstruct
+    public void started() {
+        // 애플리케이션의 기본 시간대를 한국(KST)으로 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Teamproject2GreenUniApplication.class, args);
--- src/test/java/com/green/university/Teamproject2GreenUniApplicationTests.java
+++ src/test/java/com/green/university/Teamproject2GreenUniApplicationTests.java
@@ -13,5 +13,11 @@ class Teamproject2GreenUniApplicationTests {
     void contextLoads() {
     }
 
+    // 한국시간
+    @PostConstruct
+    public void started() {
+        // 애플리케이션의 기본 시간대를 한국(KST)으로 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
 }
